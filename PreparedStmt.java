a40b598fe9709cb10d7f69d632ff1cb59b7f4b79
ashish-v-ai <ashishvekariya165@gmail.com>
2026-07-09 20:03:31 +0530

    add comment

--- PreparedStmt.java
+++ PreparedStmt.java
@@ -4,34 +4,44 @@ public class preparedStmt {
 
 	public static void main(String[] args) {
 		try {
+
+			//com.mysql.cj.jdbc.Driver is driver name for sql driver
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			System.out.println("Driver Connected");
 			
-			//connection
 			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/college","root","");
-			
 			System.out.println("Connection ok");
 			
-			//create statement
+			//Create a statement class object for executing queries on db
 			Statement stmt = conn.createStatement();
 			
+			//ResultSet is used to stroe fetched data from table
 			ResultSet rs1 = stmt.executeQuery("SELECT * from student");
-			
 			while(rs1.next()) {
 				System.out.println("id = " + rs1.getInt("id") + " | name = " + rs1.getString("sname"));
 			}
 			
+			//simple sql query
 			String sql = "select * from student where sname = ?";
+
+			//The PreparedStatement class is used to create dynamic queries
+			//The pstmt is way to run query by using user input
 			PreparedStatement pstmt = conn.prepareStatement(sql);
+
+			//The pstmt contains (?) and called 
+			//The value of (?) is given by pstmt.setXXX(sequenceNum, value);
 			String sname = "ashish";
-			pstmt.setString(1, sname);
+			pstmt.setString(1, sname); // 1 --> sequenceNum, sname --> value
+			//The (?) works in sequence like if there is three (?,?,?) so the value will be given in squence as well
 			
+			//Used to fetch rows from table based on query
 			ResultSet rs2 = pstmt.executeQuery();
 			
 			while(rs2.next()) {
 				System.out.println("id = " + rs2.getInt("id") + " | name = " + rs2.getString("sname"));
 			}
 			
+			//It essential to close all opened connection to save resources.
 			rs1.close(); stmt.close();
 			rs2.close(); pstmt.close();
 			conn.close();
