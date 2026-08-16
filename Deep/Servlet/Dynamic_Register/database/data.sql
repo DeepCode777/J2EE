dd5e568854fbd9558e416681d37736ae540683bf
Deep Poptani <deeppoptani777@gmail.com>
2026-08-16 22:46:00 +0530

    Add registration and login functionality with corresponding servlets and HTML forms

---
+++ Deep/Servlet/Dynamic_Register/database/data.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM assignment.register
+LIMIT 0, 1000
+
+-- Date: 2026-08-16 15:59
+*/
+INSERT INTO `` (`id`,`emal`,`password`,`fullname`,`gender`,`adderss`,`city`,`country`,`dob`) VALUES (1,'demo@gmail.com','1234','DB Deom','male','Amaranagar','Rajkot','India','1995-08-16');
+INSERT INTO `` (`id`,`emal`,`password`,`fullname`,`gender`,`adderss`,`city`,`country`,`dob`) VALUES (2,'deep@gmail.com','123','Deep','male','k gundala','Rajkot','india','2005-07-26');
